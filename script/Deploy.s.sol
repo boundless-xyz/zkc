58c7682102da27c8e308962856d281ea2bf4cbe1
Pote <81638931+willpote@users.noreply.github.com>
2025-05-06 16:02:56 -0400

    Update script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,22 +4,24 @@ pragma solidity ^0.8.13;
 import {Script, console2} from "forge-std/Script.sol";
 import {ZKC} from "../src/ZKC.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
 
 /**
  * Sample Usage (addresses are Anvil default accounts):
  *
- * export INITIAL_MINTER_1="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
- * export INITIAL_MINTER_2="0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
- * export TOTAL_INITIAL_SUPPLY="1000000000000000000000000000"
- * export INITIAL_MINTER_1_AMOUNT="550000000000000000000000000"
- * export INITIAL_MINTER_2_AMOUNT="450000000000000000000000000"
- * export SALT="0x0000000000000000000000000000000000000000000000000000000000000001"
- * export OWNER="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
- *
- * forge script script/Deploy.s.sol:DeployZKC \
- * --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
- * --broadcast \
- * --rpc-url http://127.0.0.1:8545
+
+export ADMIN="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+export INITIAL_MINTER_1="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+export INITIAL_MINTER_2="0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
+export TOTAL_INITIAL_SUPPLY="1000000000000000000000000000"
+export INITIAL_MINTER_1_AMOUNT="550000000000000000000000000"
+export INITIAL_MINTER_2_AMOUNT="450000000000000000000000000"
+export SALT="0x0000000000000000000000000000000000000000000000000000000000000001"
+
+forge script script/Deploy.s.sol:DeployZKC \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
+    --broadcast \
+    --rpc-url http://127.0.0.1:8545
  */
 contract DeployZKC is Script {
     function setUp() public {}
@@ -34,20 +36,48 @@ contract DeployZKC is Script {
         uint256 initialMinter2Amount = vm.envUint("INITIAL_MINTER_2_AMOUNT");
         require(totalInitialSupply == initialMinter1Amount + initialMinter2Amount);
         bytes32 salt = vm.envBytes32("SALT");
-        address owner = vm.envAddress("OWNER");
+        address admin = vm.envAddress("ADMIN");
 
-        address implementation = address(new ZKC());
+        address implementation = address(new ZKC{salt: 0x0}());
         console2.log("Deployed ZKC implementation to: ", implementation);
+
+        bytes32 initCodeHash = keccak256(abi.encodePacked(
+            type(ERC1967Proxy).creationCode,
+            abi.encode(
+                implementation,
+                abi.encodeCall(
+                    ZKC.initialize,
+                    (initialMinter1, initialMinter2, initialMinter1Amount, initialMinter2Amount, admin)
+                )
+            )
+        ));
+        console2.log("initCodeHash: ");
+        console2.logBytes32(initCodeHash);
+
+        // Deploy proxy with CREATE2
         ERC1967Proxy proxy = new ERC1967Proxy{salt: salt}(
             implementation,
             abi.encodeCall(
-                ZKC.initialize, (initialMinter1, initialMinter2, initialMinter1Amount, initialMinter2Amount, owner)
+                ZKC.initialize,
+                (initialMinter1, initialMinter2, initialMinter1Amount, initialMinter2Amount, admin)
             )
         );
         address zkc = address(proxy);
-        console2.log("Deployed ZKC to: ", zkc);
-        console2.logBytes32(address(proxy).codehash);
 
         vm.stopBroadcast();
+
+        // Sanity checks.
+        ZKC zkcContract = ZKC(zkc);
+        IAccessControl accessControl = IAccessControl(zkc);
+        console2.log("Admin address: ", admin);
+        console2.log("Admin role assigned: ", accessControl.hasRole(zkcContract.ADMIN_ROLE(), admin));
+        console2.log("Initial Minter 1: ", zkcContract.initialMinter1());
+        console2.log("Initial Minter 2: ", zkcContract.initialMinter2());
+        console2.log("Initial Minter 1 Amount: ", zkcContract.initialMinter1Remaining());
+        console2.log("Initial Minter 1 Amount Value: ", zkcContract.initialMinter1Remaining() / 10 ** 18);
+        console2.log("Initial Minter 2 Amount: ", zkcContract.initialMinter2Remaining());
+        console2.log("Initial Minter 2 Amount Value: ", zkcContract.initialMinter2Remaining() / 10 ** 18);
+        console2.log("================================================");
+        console2.log("Deployed ZKC to: ", zkc);
     }
 }
