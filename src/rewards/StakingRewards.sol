ceac08e0fba4d70e4003db372cffeeb6d4cc0c4a
Angelo Capossele <angelo@risc0.com>
2025-08-18 15:44:29 +0100

    staking rewards (#6)
    * add initial impl
    
    * fix delegate
    
    * more tests
    
    * fix
    
    * improve comments
    
    * fix event
    
    * fix errors
    
    * drop comment
    
    * revert adjustAmount
    
    * improve test
    
    * feedback

--- src/rewards/StakingRewards.sol
+++ src/rewards/StakingRewards.sol
@@ -5,11 +5,11 @@ import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/acce
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 import {ZKC} from "../ZKC.sol";
 import {IRewards} from "../interfaces/IRewards.sol";
+
+/// @notice Error thrown when a user tries to claim rewards for an epoch they have already claimed
+error AlreadyClaimed(uint256 epoch);
 
 /**
  * @title StakingRewards
@@ -17,95 +17,62 @@ import {IRewards} from "../interfaces/IRewards.sol";
  * @dev Users can claim rewards for specific epochs based on their staking value
  */
 contract StakingRewards is Initializable, AccessControlUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable {
-    using SafeERC20 for IERC20;
-
     bytes32 public constant ADMIN_ROLE = DEFAULT_ADMIN_ROLE;
-    
+
     /// @notice ZKC token contract
     ZKC public zkc;
-    
+
     /// @notice veZKC rewards interface for getting staking positions
     IRewards public veZKC;
-    
+
+    /// @dev Mapping to track if a user has claimed rewards for an epoch
+    mapping(uint256 => mapping(address => bool)) private _userClaimed;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
-    
-    function initialize(
-        address _zkc,
-        address _veZKC,
-        address _admin
-    ) public initializer {
+
+    function initialize(address _zkc, address _veZKC, address _admin) public initializer {
         __AccessControl_init();
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
-        
+
         zkc = ZKC(_zkc);
         veZKC = IRewards(_veZKC);
-        
+
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
-    
+
     /**
-     * @notice Claim rewards for a specific epoch
-     * @param epoch The epoch to claim rewards for
+     * @notice Claim rewards for the given epochs
+     * @param epochs The epochs to claim rewards for
      * @return amount The amount of rewards claimed
      */
-    function claimRewards(uint256 epoch) 
-        external 
-        nonReentrant 
-        returns (uint256 amount) 
-    {
-        revert("Not implemented");
+    function claimRewards(uint256[] calldata epochs) external nonReentrant returns (uint256 amount) {
+        return _claim(msg.sender, epochs);
     }
-    
+
     /**
-     * @notice Claim rewards for multiple epochs in a single transaction
-     * @param epochs Array of epochs to claim rewards for
-     * @return totalAmount Total amount of rewards claimed across all epochs
-     */
-    function batchClaimRewards(uint256[] calldata epochs) 
-        external 
-        nonReentrant 
-        returns (uint256 totalAmount) 
-    {
-        revert("Not implemented");
-    }
-    
-    /**
-     * @notice Calculate the rewards a user is owed for a specific epoch
+     * @notice Calculate the rewards a user is owed for the given epochs
      * @param user The user address
-     * @param epoch The epoch to calculate rewards for
-     * @return amount The amount of rewards owed
+     * @param epochs The epochs to calculate rewards for
+     * @return rewards The rewards owed
      */
-    function calculateRewardsForEpoch(address user, uint256 epoch) 
-        external 
-        view 
-        returns (uint256 amount) 
-    {
-        // TODO: Implement reward calculation logic
-        // 1. Get user's staking value for this epoch via IRewards interface
-        // 2. Calculate proportional share of epoch's staking allocation
-        // 3. Return calculated amount
-        
-        revert("Not implemented");
+    function calculateRewards(address user, uint256[] calldata epochs) external returns (uint256[] memory) {
+        return _calculate(user, epochs);
     }
-    
+
     /**
      * @notice Check if a user has claimed rewards for a specific epoch
      * @param user The user address
      * @param epoch The epoch to check
      * @return claimed Whether rewards have been claimed
      */
-    function hasUserClaimedRewards(address user, uint256 epoch) 
-        external 
-        view 
-        returns (bool claimed) 
-    {
-        revert("Not implemented");
+    function hasUserClaimedRewards(address user, uint256 epoch) external view returns (bool claimed) {
+        return _userClaimed[epoch][user];
     }
-    
+
     /**
      * @notice Get the current epoch from the ZKC contract
      * @return currentEpoch The current epoch number
@@ -113,6 +80,64 @@ contract StakingRewards is Initializable, AccessControlUpgradeable, UUPSUpgradea
     function getCurrentEpoch() external view returns (uint256 currentEpoch) {
         return zkc.getCurrentEpoch();
     }
-    
+
+    /**
+     * @notice Get the end timestamp for a specific epoch
+     * @param epoch The epoch number
+     * @return endTimestamp The end timestamp of the epoch
+     */
+    function _epochEndTimestamp(uint256 epoch) internal view returns (uint256) {
+        return zkc.getEpochStartTime(epoch + 1);
+    }
+
+    /**
+     * @notice Internal function to calculate the rewards a user is owed for the given epochs
+     * @param user The user address
+     * @param epochs The epochs to calculate rewards for
+     * @return rewards The list of rewards owed
+     */
+    function _calculate(address user, uint256[] calldata epochs) internal returns (uint256[] memory) {
+        uint256 currentEpoch = zkc.getCurrentEpoch();
+        uint256[] memory rewards = new uint256[](epochs.length);
+        for (uint256 i = 0; i < epochs.length; i++) {
+            uint256 epoch = epochs[i];
+            if (epoch >= currentEpoch) continue; // cannot claim ongoing/future epoch
+            uint256 snapshotTime = _epochEndTimestamp(epoch);
+            uint256 userPower = veZKC.getPastRewards(user, snapshotTime);
+            if (userPower == 0) continue;
+            uint256 totalPower = veZKC.getPastTotalRewards(snapshotTime);
+            if (totalPower == 0) continue;
+            uint256 emission = zkc.getStakingEmissionsForEpoch(epoch);
+            rewards[i] = (emission * userPower) / totalPower;
+        }
+        return rewards;
+    }
+
+    /**
+     * @notice Internal function to claim rewards for a user in the given epochs
+     * @param user The user address
+     * @param epochs The epochs to claim rewards for
+     * @return amount The amount of rewards claimed
+     */
+    function _claim(address user, uint256[] calldata epochs) internal returns (uint256 amount) {
+        uint256[] memory amounts = _calculate(user, epochs);
+        uint256 currentEpoch = zkc.getCurrentEpoch();
+        for (uint256 i = 0; i < epochs.length; i++) {
+            uint256 epoch = epochs[i];
+            if (_userClaimed[epoch][user]) revert AlreadyClaimed(epoch);
+            // Epoch must have ended
+            if (epoch >= currentEpoch) revert ZKC.EpochNotEnded(epoch);
+            _userClaimed[epoch][user] = true;
+            amount += amounts[i];
+        }
+        if (amount == 0) return 0;
+        zkc.mintStakingRewardsForRecipient(user, amounts, epochs);
+        return amount;
+    }
+
+    /**
+     * @notice Authorize upgrades to this contract
+     * @param newImplementation The address of the new implementation
+     */
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(ADMIN_ROLE) {}
 }
---
+++ test/StakingRewards.t.sol
@@ -0,0 +1,277 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {StakingRewards, AlreadyClaimed} from "../src/rewards/StakingRewards.sol";
+import {veZKC} from "../src/veZKC.sol";
+import {ZKC} from "../src/ZKC.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract StakingRewardsTest is Test {
+    ZKC internal zkc;
+    veZKC internal vezkc;
+    StakingRewards internal rewards;
+
+    address internal admin = address(0xA11CE);
+    address internal minter1 = address(0x1001);
+    address internal minter2 = address(0x1002);
+    address internal user1 = address(0xBEEF1);
+    address internal user2 = address(0xBEEF2);
+
+    uint256 internal EPOCH_DURATION;
+
+    function setUp() public {
+        vm.startPrank(admin);
+        ZKC zkcImpl = new ZKC();
+        uint256 supply = zkcImpl.INITIAL_SUPPLY();
+        bytes memory zkcInit = abi.encodeCall(ZKC.initialize, (minter1, minter2, supply / 2, supply / 2, admin));
+        zkc = ZKC(address(new ERC1967Proxy(address(zkcImpl), zkcInit)));
+        zkc.initializeV2();
+        EPOCH_DURATION = zkc.EPOCH_DURATION();
+
+        veZKC veImpl = new veZKC();
+        bytes memory veInit = abi.encodeCall(veZKC.initialize, (address(zkc), admin));
+        vezkc = veZKC(address(new ERC1967Proxy(address(veImpl), veInit)));
+
+        StakingRewards srImpl = new StakingRewards();
+        bytes memory srInit = abi.encodeCall(StakingRewards.initialize, (address(zkc), address(vezkc), admin));
+        rewards = StakingRewards(address(new ERC1967Proxy(address(srImpl), srInit)));
+
+        zkc.grantRole(zkc.STAKING_MINTER_ROLE(), address(rewards));
+        vm.stopPrank();
+
+        deal(address(zkc), user1, 1_000_000e18);
+        deal(address(zkc), user2, 1_000_000e18);
+
+        vm.prank(user1);
+        zkc.approve(address(vezkc), type(uint256).max);
+        vm.prank(user2);
+        zkc.approve(address(vezkc), type(uint256).max);
+        vm.warp(block.timestamp + 1);
+    }
+
+    // Helper functions for staking
+    function _stake(address user, uint256 amount) internal {
+        vm.prank(user);
+        vezkc.stake(amount, type(uint256).max); // max lock
+    }
+
+    // Helper function to fast-forward epochs
+    function _endEpochs(uint256 n) internal {
+        vm.warp(block.timestamp + n * EPOCH_DURATION);
+    }
+
+    // Internal function to claim rewards for a user and epochs
+    function _claimRewards(address user, uint256[] memory epochs) internal returns (uint256) {
+        uint256[] memory amounts = rewards.calculateRewards(user, epochs);
+        vm.expectEmit(true, true, true, true);
+        emit ZKC.StakingRewardsClaimed(user, epochs, amounts);
+        vm.prank(user);
+        uint256 amount = rewards.claimRewards(epochs);
+        return amount;
+    }
+
+    // Helper function to claim rewards for a user in a single epoch
+    function _claimRewardsForEpoch(address user, uint256 epoch) internal returns (uint256) {
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = epoch;
+        return _claimRewards(user, epochs);
+    }
+
+    // Test single user gets full emission
+    function testSingleUserGetsFullEmission() public {
+        _stake(user1, 100e18);
+        _endEpochs(1);
+        uint256 claimed = _claimRewardsForEpoch(user1, 0);
+        assertEq(claimed, zkc.getStakingEmissionsForEpoch(0));
+    }
+
+    // Test two users split pro-rata
+    function testTwoUsersSplitProRata() public {
+        _stake(user1, 100e18);
+        _stake(user2, 300e18);
+        _endEpochs(1);
+        uint256 c1 = _claimRewardsForEpoch(user1, 0);
+        uint256 c2 = _claimRewardsForEpoch(user2, 0);
+        uint256 total = zkc.getStakingEmissionsForEpoch(0);
+        assertEq(c1 + c2, total);
+        assertApproxEqRel(c1, total * 100 / 400, 1e16);
+        assertApproxEqRel(c2, total * 300 / 400, 1e16);
+    }
+
+    // Test cannot claim rewards for future epoch
+    function testCannotClaimFutureEpoch() public {
+        _stake(user1, 50e18);
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = 0;
+        vm.expectRevert(abi.encodeWithSelector(ZKC.EpochNotEnded.selector, 0));
+        vm.prank(user1);
+        rewards.claimRewards(epochs);
+    }
+
+    // Test cannot claim rewards for an epoch that has already been claimed
+    function testCannotDoubleClaim() public {
+        _stake(user1, 50e18);
+        _endEpochs(1);
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = 0;
+        vm.prank(user1);
+        rewards.claimRewards(epochs);
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(AlreadyClaimed.selector, 0));
+        rewards.claimRewards(epochs);
+    }
+
+    // Test batch claim for multiple epochs
+    function testBatchClaim() public {
+        _stake(user1, 50e18);
+        _endEpochs(3);
+        uint256[] memory epochs = new uint256[](3);
+        epochs[0] = 0;
+        epochs[1] = 1;
+        epochs[2] = 2;
+        uint256 totalClaim = _claimRewards(user1, epochs);
+        uint256 expected;
+        expected += zkc.getStakingEmissionsForEpoch(0);
+        expected += zkc.getStakingEmissionsForEpoch(1);
+        expected += zkc.getStakingEmissionsForEpoch(2);
+        assertEq(totalClaim, expected);
+    }
+
+    // Delegation should not transfer reward power to delegatee.
+    function testDelegationDoesNotAffectRewards() public {
+        _stake(user1, 100e18);
+        _stake(user2, 300e18);
+        // Delegate voting power from user1 to user2
+        vm.prank(user1);
+        vezkc.delegate(user2);
+        _endEpochs(1);
+        uint256 emission = zkc.getStakingEmissionsForEpoch(0);
+        uint256 c1 = _claimRewardsForEpoch(user1, 0);
+        uint256 c2 = _claimRewardsForEpoch(user2, 0);
+        assertEq(c1 + c2, emission, "Total emission mismatch");
+        uint256 exp1 = (emission * 100) / 400; // 1/4
+        uint256 exp2 = emission - exp1; // 3/4
+        assertApproxEqRel(c1, exp1, 1e16, "Delegation changed delegator rewards");
+        assertApproxEqRel(c2, exp2, 1e16, "Delegatee improperly gained reward power");
+        // checking voting power
+        uint256 user1VotingPower = vezkc.getVotes(user1);
+        uint256 user2VotingPower = vezkc.getVotes(user2);
+        assertEq(user1VotingPower, 0, "User1 should have no voting power");
+        assertEq(user2VotingPower, 400, "User2 should have full voting power");
+    }
+
+    // Claiming an epoch with no stake should mint nothing, mark claimed, and block later claims.
+    function testClaimZeroNoStake() public {
+        _endEpochs(1); // finish epoch 0 with no stakers
+        uint256 balBefore = zkc.balanceOf(user1);
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = 0;
+        vm.prank(user1);
+        uint256 claimed = rewards.claimRewards(epochs);
+        assertEq(claimed, 0);
+        assertEq(zkc.balanceOf(user1), balBefore, "Balance changed despite zero claim");
+        // Second attempt reverts because marked claimed even if zero
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(AlreadyClaimed.selector, 0));
+        rewards.claimRewards(epochs);
+    }
+
+    // Mixed past + future epochs in one batch should revert on the future epoch.
+    function testCannotClaimBatchContainingFutureEpoch() public {
+        _stake(user1, 100e18);
+        _endEpochs(2); // epochs 0 and 1 finished; currentEpoch == 2
+        uint256[] memory epochs = new uint256[](2);
+        epochs[0] = 0;
+        epochs[1] = 2; // future
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(ZKC.EpochNotEnded.selector, 2));
+        rewards.claimRewards(epochs);
+    }
+
+    // Duplicate epoch in the same batch should revert on second encounter.
+    function testDuplicateEpochInSameBatchReverts() public {
+        _stake(user1, 50e18);
+        _endEpochs(1);
+        uint256[] memory epochs = new uint256[](2);
+        epochs[0] = 0;
+        epochs[1] = 0; // duplicate
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(AlreadyClaimed.selector, 0));
+        rewards.claimRewards(epochs);
+    }
+
+    // Calling calculateRewards must not mark epochs as claimed.
+    function testCalculateDoesNotAffectClaimStatus() public {
+        _stake(user1, 80e18);
+        _endEpochs(1);
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = 0;
+        rewards.calculateRewards(user1, epochs); // should not affect
+        assertFalse(rewards.hasUserClaimedRewards(user1, 0));
+        vm.prank(user1);
+        rewards.claimRewards(epochs); // should succeed
+    }
+
+    // User staking after an epoch ends cannot retroactively earn that epoch; claim marks it and blocks later retroactive attempts.
+    function testNoRetroactiveEarningForPastEpoch() public {
+        _endEpochs(1); // epoch 0 ends with no stake
+        _stake(user1, 100e18); // stake in epoch 1
+        uint256[] memory epochs = new uint256[](1);
+        epochs[0] = 0; // past epoch with zero power
+        vm.prank(user1);
+        uint256 claimed = rewards.claimRewards(epochs);
+        assertEq(claimed, 0);
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(AlreadyClaimed.selector, 0));
+        rewards.claimRewards(epochs);
+    }
+
+    // Unsorted epoch list should still succeed and sum emissions correctly.
+    function testUnsortedEpochBatchClaim() public {
+        _stake(user1, 120e18);
+        _endEpochs(3); // epochs 0,1,2
+        uint256[] memory epochs = new uint256[](3);
+        epochs[0] = 2;
+        epochs[1] = 0;
+        epochs[2] = 1;
+        uint256 expected =
+            zkc.getStakingEmissionsForEpoch(0) + zkc.getStakingEmissionsForEpoch(1) + zkc.getStakingEmissionsForEpoch(2);
+        uint256[] memory calc = rewards.calculateRewards(user1, epochs);
+        uint256 sum;
+        for (uint256 i; i < calc.length; ++i) {
+            sum += calc[i];
+        }
+        vm.prank(user1);
+        uint256 claimed = rewards.claimRewards(epochs);
+        assertEq(claimed, sum, "Claimed != calculated sum");
+        assertEq(claimed, expected, "Claimed != expected emissions (single staker)");
+    }
+
+    // Gas Benchmarks
+    function _runBatchGas(uint256 epochsToSimulate, uint256 stakeAmount, string memory label) internal {
+        _stake(user1, stakeAmount);
+        _endEpochs(epochsToSimulate);
+        uint256[] memory epochs = new uint256[](epochsToSimulate);
+        for (uint256 i; i < epochsToSimulate; ++i) {
+            epochs[i] = i;
+        }
+        vm.prank(user1);
+        uint256 total = rewards.claimRewards(epochs);
+        vm.snapshotGasLastCall(label);
+        assertGt(total, 0);
+    }
+
+    function testGas_SingleClaim() public {
+        _runBatchGas(1, 100e18, "stake claim: single");
+    }
+
+    function testGas_BatchClaim15Epochs() public {
+        _runBatchGas(15, 150e18, "stake claim: batch15");
+    }
+
+    function testGas_BatchClaim30Epochs() public {
+        _runBatchGas(30, 300e18, "stake claim: batch30");
+    }
+}
